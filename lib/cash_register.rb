987be0ea4cc8606cf8ceb8df5c1e340136842fd0
Michael Boroski <mjboroski@gmail.com>
2018-04-23 20:20:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class Cash_Register
 
-  attr_accessor :total, :employee_discount
+  attr_accessor :total, :employee_discount, :items
 
   def  initialize(employee_discount = 0)
     @total=0
@@ -12,12 +12,12 @@ class Cash_Register
   end
 
   def add_item(title, price)
-
-
+    @total += price
+    @items << title
   end
 
   def apply_discount
-
+    @total -= @employee_discount
   end
 
   def items
