529cde33fee96c63332132b8654d0a6b5944193d
Michael Boroski <mjboroski@gmail.com>
2018-04-23 20:18:36 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class Cash_Register
 
+  attr_accessor :total, :employee_discount
+
+  def  initialize(employee_discount = 0)
+    @total=0
+    @employee_discount = employee_discount
+  end
+
+  def total
+    @total
+  end
+  
+  def add_item(title, price)
+    
+
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+  
+  def void_last_transaction
+
+  end
+ end
